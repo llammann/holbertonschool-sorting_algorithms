adede63aefc2af399907a4cea5325fff04c41a50
llammann <bd7n2v9tr@code.edu.az>
2024-04-11 20:01:49 +0000

    Bubble sort

--- sort.h
+++ sort.h
@@ -1,6 +1,15 @@
 #ifndef SORT_H
 #define SORT_H
 
+#include <stddef.h>
+
+typedef struct listint_s
+{
+int n;
+struct listint_s *next;
+} listint_t;
+
+
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
