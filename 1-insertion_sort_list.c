63da75dcc829edd689ea47b33319dae4c52aa568
rufatjabbarov22 <rufatjabbarov001@gmail.com>
2024-04-12 10:43:45 +0000

    42

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -3,7 +3,7 @@
 #include "sort.h"
 
 /**
-*insertion_sort_list - Sorts a doubly linked list of integers
+* insertion_sort_list - Sorts a doubly linked list of integers
 *                       in ascending order using the Insertion sort algorithm
 *@list: Pointer to the head of the list
 */
@@ -13,9 +13,7 @@ void insertion_sort_list(listint_t **list)
     if (list == NULL || *list == NULL || (*list)->next == NULL)
         return;
 
-    listint_t *current;
-    listint_t *prev_node;
-
+    listint_t *current, *prev_node;
     current = (*list)->next;
 
     while (current != NULL)
