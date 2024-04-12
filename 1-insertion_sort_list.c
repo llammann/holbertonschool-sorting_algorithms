9475c45866eea604295b633cb9d2ea4e27aa4fc1
rufatjabbarov22 <rufatjabbarov001@gmail.com>
2024-04-12 10:13:05 +0000

    42

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,37 +1,62 @@
 #include "sort.h"
 
 /**
-*insertion_sort_list - sorts a doubly linked list of integers
+*insertion_sort_list - Sorts a doubly linked list of integers
 *                       in ascending order using the Insertion sort algorithm
-*@list: pointer to the pointer to the head of the list
+*@list: Pointer to the head of the doubly linked list
 */
 
 void insertion_sort_list(listint_t **list)
 {
-    listint_t *current, *prev_node;
-
     if (list == NULL || *list == NULL || (*list)->next == NULL)
         return;
 
-    current = (*list)->next;
-    while (current)
+    listint_t *current = (*list)->next;
+
+    while (current != NULL)
     {
-        prev_node = current->prev;
-        while (prev_node && prev_node->n > current->n)
+        listint_t *insertion_point = current->prev;
+        listint_t *next_node = current->next;
+
+        while (insertion_point != NULL && insertion_point->n > current->n)
         {
-            if (prev_node->prev)
-                prev_node->prev->next = current;
-            else
-                *list = current;
-            if (current->next)
-                current->next->prev = prev_node;
-            prev_node->next = current->next;
-            current->prev = prev_node->prev;
-            current->next = prev_node;
-            prev_node->prev = current;
-            print_list(*list);
-            prev_node = current->prev;
+            insertion_point = insertion_point->prev;
         }
-        current = current->next;
+
+        if (insertion_point == NULL)
+        {
+            if (current->prev != NULL)
+                current->prev->next = current->next;
+
+            if (current->next != NULL)
+                current->next->prev = current->prev;
+
+            current->prev = NULL;
+            current->next = *list;
+
+            if (*list != NULL)
+                (*list)->prev = current;
+
+            *list = current;
+        }
+        else
+        {
+            if (current->prev != NULL)
+                current->prev->next = current->next;
+
+            if (current->next != NULL)
+                current->next->prev = current->prev;
+
+            current->prev = insertion_point;
+
+            if (insertion_point->next != NULL)
+                insertion_point->next->prev = current;
+
+            current->next = insertion_point->next;
+            insertion_point->next = current;
+        }
+
+        current = next_node;
+        print_list(*list);
     }
 }
